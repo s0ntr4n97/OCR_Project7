a2b33ce565935157776df38dfa3324185a0112c8
tranlam.son <tranlam.son@bigtreetc.com>
2021-01-26 18:55:57 +0700

    commit

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,8 +1,9 @@
 import React, { Fragment, useEffect, useState } from 'react';
-import { MapContainer, Marker, TileLayer, useMapEvents } from 'react-leaflet';
-import { makeStyles } from '@material-ui/core';
-import { Popup } from 'leaflet';
-
+import { MapContainer, Marker, TileLayer, useMapEvents, Popup } from 'react-leaflet';
+import { Input, makeStyles, TextField, Typography } from '@material-ui/core';
+import L from 'leaflet';
+import customIcon from './marker.svg'
+import restIcon from './restaurant.svg'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -12,46 +13,76 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-function LocationMarker() {
-  const [position, setPosition] = useState(null);
+function LocationMarker(props) {
+
   const map = useMapEvents({
-    // click() {
-    //   map.locate();
-    // },
     locationfound(e) {
-      // setPosition(e.latlng);
       map.flyTo(e.latlng, map.getZoom());
+      props.setPosition({ lat: e.latlng.lat, lng: e.latlng.lng })
     },
 
   });
 
-  useEffect(() => {
-    map.locate();
-  });
-
-  return position === null ? null : (
-    <Marker position={position}>
+  return (
+    <Marker position={props.position}>
       <Popup>You are here</Popup>
     </Marker>
   )
 
-
-
 }
+
+function AddRestaurant() {
+  const [position, setPosition] = useState(null);
+
+  const map = useMapEvents({
+    click(e) {
+      setPosition(e.latlng);
+    }
+  });
+
+  return position === null ? null : (
+    <Marker position={position}>
+    </Marker>
+  )
+}
+
+
+// const personIcon = new L.Icon({
+//   iconUrl: customIcon,
+//   iconRetinaUrl: customIcon,
+//   iconAnchor: [32, 64],
+//   iconSize: new L.Point(40, 55),
+//   className: 'leaflet-div-icon'
+// });
+
+
+// const RestaurantIcon = new L.icon({
+//   iconUrl: restIcon,
+//   iconRetinaUrl: restIcon,
+//   iconAnchor: [32, 64],
+//   iconSize: new L.Point(40, 55),
+//   className: 'leaflet-div-icon'
+// })
 
 
 export default function Map(props) {
   const classes = useStyles()
 
   return (
-    <MapContainer className={classes.root} center={[props.location.lat, props.location.long]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer className={classes.root} center={[props.position.lat, props.position.lng]} zoom={13}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <LocationMarker />
-
-      {/* { props.restaurants.map((rest, idx) => <Marker key={idx} position={[rest.lat, rest.long]} />)} */}
+      <LocationMarker setPosition={props.setPosition} position={props.position} />
+      <AddRestaurant />
+      { props.restaurants.map((rest, idx) =>
+        <Marker key={idx} position={[rest.lat, rest.lng]}>
+          <Popup>
+            <Typography variant="h6">{rest.restaurantName}</Typography>
+          </Popup>
+        </Marker>
+      )}
     </MapContainer>
   )
 }
--- src/components/restaurants/ListRestaurants.js
+++ src/components/restaurants/ListRestaurants.js
@@ -1,4 +1,4 @@
-import { Grid, makeStyles, TextField, Typography } from '@material-ui/core';
+import { Button, Grid, makeStyles, TextField, Typography } from '@material-ui/core';
 import React from 'react';
 import RestaurantBox from './RestaurantBox';
 
@@ -33,6 +33,7 @@ function ListRestaurants(props) {
         <TextField type="number" name="starTo" className={classes.starInput} InputProps={{ inputProps: { min: 0, max: 5 } }} />
         <Typography variant="body1" display="inline" className={classes.labelSelector}>To</Typography>
         <TextField type="number" name="starFrom" className={classes.starInput} InputProps={{ inputProps: { min: 0, max: 5 } }} />
+        <Button variant="contained" size="small">Find</Button>
       </Grid>
       <Grid container className={classes.root}>
         {props.restaurants.map((rest, idx) => <RestaurantBox key={idx} rest={rest} />)}
